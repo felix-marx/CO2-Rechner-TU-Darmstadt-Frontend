33a8d8c6ed216615141df5ee0e880ba26ae17ac5
Lithium-1Hauptgruppe <62305832+Lithium-1Hauptgruppe@users.noreply.github.com>
2021-11-29 16:22:10 +0100

    style(Mitarbeiterumfrage) Tooltip and Align
    Fixed Tooltip position in title.
    Placed every row in container for alignment.

--- src/components/mitarbeiterUmfrage.vue
+++ src/components/mitarbeiterUmfrage.vue
@@ -3,45 +3,56 @@
     <v-card elevation="2" outlined>
       <v-form>
         <v-card class="pa-7">
+           <h3>Wie kommen Sie ins Büro?
             <v-tooltip bottom>
-                <template v-slot:activator="{ on, attrs }">
-                    <span v-bind="attrs" v-on="on"><h3>Wie kommen Sie ins Büro?</h3></span>
+                <template v-slot:activator="{ on }">
+                  <v-icon v-on="on">
+                    mdi-help-circle-outline
+                  </v-icon>
                 </template>
-                <span>Die Distanz können Sie zum Beispiel mit Google Maps bestimmen</span>
-            </v-tooltip>
+                Die Distanz können Sie zum Beispiel mit Google Maps bestimmen.
+              </v-tooltip>
+            </h3>
             <v-divider></v-divider>
             <br>
-
-          <v-row>
-              <v-select v-model="verkehrsmittel1" :items="fahrtmediumListe" label="Verkehrsmedium"></v-select>
-              <v-select v-model="verkehrsmittel2" v-show="verkehrsmittel1 === 'Öffentliche'" :items="fahrtmediumÖPNVListe" label="ÖPNV"></v-select>
-              <v-text-field v-model="einfacherPendelweg" label="Einfacher Pendelweg" type=number suffix="km"></v-text-field>
-          </v-row>
-          <p v-show="verkehrsmittel1 === 'PKW'">Fahren Sie in einer Fahrgemeinschaft?</p>
-          <v-row>
-              <v-checkbox v-show="verkehrsmittel1 === 'PKW'" v-model="activeFahrgemeinschaft" label="Ja"></v-checkbox>
-              <v-text-field v-model="fahrgemeinschaft" v-show="activeFahrgemeinschaft" label="Fahrgemeinschaftmitglieder" type="number" class="pr-5"></v-text-field>
-          </v-row>
+          
+          <v-container>
+            <v-row>
+                <v-select v-model="verkehrsmittel1" :items="fahrtmediumListe" label="Verkehrsmedium"></v-select>
+                <v-select v-model="verkehrsmittel2" v-show="verkehrsmittel1 === 'Öffentliche'" :items="fahrtmediumÖPNVListe" label="ÖPNV"></v-select>
+                <v-text-field v-model="einfacherPendelweg" label="Einfacher Pendelweg" type=number suffix="km"></v-text-field>
+            </v-row>
+            <p v-show="verkehrsmittel1 === 'PKW'">Fahren Sie in einer Fahrgemeinschaft?</p>
+            <v-row>
+                <v-checkbox v-show="verkehrsmittel1 === 'PKW'" v-model="activeFahrgemeinschaft" label="Ja"></v-checkbox>
+                <v-text-field v-model="fahrgemeinschaft" v-show="activeFahrgemeinschaft" label="Fahrgemeinschaftmitglieder" type="number" class="pr-5"></v-text-field>
+            </v-row>
+          </v-container>
 
           <br>
           <h3>Wie viele Tage in der Woche sind Sie im Büro?</h3>
           <v-divider></v-divider>
           <br>
-          <v-row>
-              <v-text-field v-model="arbeitstageBuero" label="Tage im Büro" type=number></v-text-field>
-          </v-row>
+
+          <v-container>
+            <v-row>
+                <v-text-field v-model="arbeitstageBuero" label="Tage im Büro" type=number></v-text-field>
+            </v-row>
+          </v-container>
 
           <br>
           <h3>Welche Dienstreisen haben Sie in den letzten 12 Monaten unternommen?</h3>
           <v-divider></v-divider>
           <br>
 
-          <v-row>
-              <v-select v-model="dienstreiseMedium" label="Verkehrsmittel" :items="dienstreiseMediumListe"></v-select>
-              <!--<v-select v-model="flugKlasse" label="Klasse" v-show="dienstreiseMedium === 'Flugzeug'" :items="flugtypListe"></v-select>-->
-              <v-select v-model="flugstreckenTyp" label="Flugstrecke" v-show="dienstreiseMedium === 'Flugzeug'" :items="flugstreckeListe"></v-select>
-              <v-text-field v-model="distanz" :disabled="(dienstreiseMedium===null)" label="einfache Distanz" suffix="km"></v-text-field>
-          </v-row>
+          <v-container>
+            <v-row>
+                <v-select v-model="dienstreiseMedium" label="Verkehrsmittel" :items="dienstreiseMediumListe"></v-select>
+                <!--<v-select v-model="flugKlasse" label="Klasse" v-show="dienstreiseMedium === 'Flugzeug'" :items="flugtypListe"></v-select>-->
+                <v-select v-model="flugstreckenTyp" label="Flugstrecke" v-show="dienstreiseMedium === 'Flugzeug'" :items="flugstreckeListe"></v-select>
+                <v-text-field v-model="distanz" :disabled="(dienstreiseMedium===null)" label="einfache Distanz" suffix="km"></v-text-field>
+            </v-row>
+          </v-container>
 
           <br>
           <h3>Welche IT-Geräte benutzen Sie in ihrer Arbeit?</h3>
@@ -72,14 +83,16 @@
                 <v-text-field v-model="mobiltelefon" :disabled="!activeMobiltelefon" label="Mobiltelefone" type="number" class="pr-5" suffix="Gerät/e"></v-text-field>
             </v-row>
           </v-container>
+
           <br>
           <h3>Schätzen Sie bitte wie viele Blätter Papier Sie in einer typischen Woche verbrauchen.</h3>
           <v-divider></v-divider>
-          <br>
-
-          <v-row>
-            <v-select v-model="papierverbrauch" :items="papierverbrauchListe" label="Papierverbrauch"></v-select>
-          </v-row>
+          
+          <v-container>
+            <v-row>
+                <v-select v-model="papierverbrauch" :items="papierverbrauchListe" label="Papierverbrauch"></v-select>
+            </v-row>
+          </v-container>
         </v-card>
       </v-form>
     </v-card>
