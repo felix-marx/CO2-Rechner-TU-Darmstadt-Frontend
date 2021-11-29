fe00ef0a0904407061b58718276bba7760be0809
Lithium-1Hauptgruppe <62305832+Lithium-1Hauptgruppe@users.noreply.github.com>
2021-11-29 15:04:05 +0100

    feat(Umfrage Hauptverantwortlicher & Mitarbeiter)
    Umfragegrundstruktur für Hauptverantwortlicher in components/umfrage.vue
    Mitarbeiter in components/mitarbeiterUmfrage.vue
    
    Gegenfalls einzelne Variablen für Textfelder noch nicht initialisiert.
    
    Kuss <3

---
+++ src/components/mitarbeiterUmfrage.vue
@@ -0,0 +1,108 @@
+<template>
+  <v-container>
+    <v-card elevation="2" outlined>
+      <v-form>
+        <v-card class="pa-7">
+            <v-tooltip bottom>
+                <template v-slot:activator="{ on, attrs }">
+                    <span v-bind="attrs" v-on="on"><h3>Wie kommen Sie ins Büro?</h3></span>
+                </template>
+                <span>Die Distanz können Sie zum Beispiel mit Google Maps bestimmen</span>
+            </v-tooltip>
+            <v-divider></v-divider>
+          <v-row>
+              <v-select v-model="verkehrsmittel1" :items="fahrtmediumListe" label="Verkehrsmedium"></v-select>
+              <v-select v-model="verkehrsmittel2" v-show="verkehrsmittel1 === 'Öffentliche'" :items="fahrtmediumÖPNVListe" label="ÖPNV"></v-select>
+              <v-text-field v-model="einfacherPendelweg" label="Einfacher Pendelweg" type=number suffix="km"></v-text-field>
+          </v-row>
+          <p v-show="verkehrsmittel1 === 'PKW'">Fahren Sie in einer Fahrgemeinschaft?</p>
+          <v-row>
+              <v-checkbox v-show="verkehrsmittel1 === 'PKW'" v-model="activeFahrgemeinschaft" label="Ja"></v-checkbox>
+              <v-text-field v-model="fahrgemeinschaft" v-show="activeFahrgemeinschaft" label="Fahrgemeinschaftmitglieder" type="number" class="pr-5"></v-text-field>
+          </v-row>
+
+          <br>
+          Wie viele Tage in der Woche sind Sie im Büro?
+          <v-divider></v-divider>
+          <br>
+          <v-row>
+              <v-text-field v-model="arbeitstageBuero" label="Tage im Büro" type=number></v-text-field>
+          </v-row>
+          Sollten Sie in den letzten 12 Monaten eine Dienstreise unternommen haben, dann tragen Sie bitte die Reiseinformationen hier ein.
+          <v-row>
+              <v-select v-model="dienstreiseMedium" label="Verkehrsmittel" :items="dienstreiseMediumListe"></v-select>
+              <!--<v-select v-model="flugKlasse" label="Klasse" v-show="dienstreiseMedium === 'Flugzeug'" :items="flugtypListe"></v-select>-->
+              <v-select v-model="flugstreckenTyp" label="Flugstrecke" v-show="dienstreiseMedium === 'Flugzeug'" :items="flugstreckeListe"></v-select>
+              <v-text-field v-model="distanz" :disabled="(dienstreiseMedium===null)" label="einfache Distanz" suffix="km"></v-text-field>
+          </v-row>
+          Wenn Sie in ihrer Arbeit IT-Geräte benutzen, wählen Sie bitte die entsprechenden Geräte an und geben Sie an wie viele Geräte dieser Art Sie benutzen.
+          <v-row>
+            <v-checkbox v-model="activeNotebook" hide-details></v-checkbox>
+            <v-text-field v-model="notebook" :disabled="!activeNotebook" label="Notebooks" type="number" class="pr-5" suffix="Stück"></v-text-field>
+          </v-row>
+          <v-row>
+            <v-checkbox v-model="activeDesktopPC" hide-details></v-checkbox>
+            <v-text-field v-model="desktopPC" :disabled="!activeDesktopPC" label="Desktop PCs" type="number" class="pr-5" suffix="Stück"></v-text-field>
+          </v-row>
+          <v-row>
+            <v-checkbox v-model="activeBildschirm" hide-details></v-checkbox>
+            <v-text-field v-model="bildschirme" :disabled="!activeBildschirm" label="Bildschirme" type="number" class="pr-5" suffix="Stück"></v-text-field>
+          </v-row>
+         <v-row>
+            <v-checkbox v-model="activeMobiltelefon" hide-details></v-checkbox>
+            <v-text-field v-model="mobiltelefon" :disabled="!activeMobiltelefon" label="Mobiltelefone" type="number" class="pr-5" suffix="Stück"></v-text-field>
+          </v-row>
+          Schätzen Sie bitte wie viele Blätter Papier Sie in einer typischen Woche verbrauchen.
+          <v-row>
+            <v-select v-model="papierverbrauch" :items="papierverbrauchListe" label="Papierverbrauch"></v-select>
+          </v-row>
+        </v-card>
+      </v-form>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    activeNotebook: false,
+    notebook: null,
+    activeDesktopPC: false,
+    desktopPC: null,
+    activeBildschirm: false,
+    bildschirme: null,
+    activeMobiltelefon: false,
+    mobiltelefon: null,
+
+    fahrtmediumListe: ['PKW', 'Fahrrad', 'E-Fahrrad', 'Motorisiertes Zweirad', 'Öffentliche'],
+    fahrtmediumÖPNVListe: ['Bahn', 'Linienbus', 'U-Bahn', 'Straßenbahn', 'MIX inkl. U-Bahn', 'MIX exkl. U-Bahn'],
+    verkehrsmittel1: null,
+    verkehrsmittel2: null,
+    activeFahrgemeinschaft: false,
+    fahrgemeinschaft: null,
+
+    dienstreiseMediumListe: ['PKW', 'Bahn', 'Fernbus', 'Flugzeug'],
+    dienstreiseMedium: null,
+    flugtypListe: ['Economy', 'Business'],
+    flugstreckeListe: ['Langstrecke', 'Kurzstrecke'],
+    flugKlasse: null,
+    
+    papierverbrauch: null,
+    papierverbrauchListe: ['0', '1-20', '21 - 50', '51 - 70', '70+']
+    })
+}
+</script>
+
+<style lang="scss">
+/* Chrome, Safari, Edge, Opera */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+/* Firefox */
+input[type=number] {
+  -moz-appearance: textfield;
+}
+</style>
---
+++ src/components/umfrage.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-container>
+    <v-card elevation="2" outlined>
+      <v-form>
+        <v-card class="pa-7" >
+          <v-row>
+            <v-text-field v-model="anzahlMitarbeiter" label="Mitarbeiteranzahl" prepend-icon="mdi-account"></v-text-field>
+          </v-row>
+          <v-row>
+            <v-text-field v-model="gebaeudeNr" label="Gebäudenr" prepend-icon="mdi-domain"></v-text-field>
+            <v-text-field v-model="flaechenanteil" label="Flächenanteil" prepend-icon="mdi-domain"></v-text-field>
+          </v-row>
+          <v-row>
+            <v-checkbox v-model="activeNetzwerkdrucker" hide-details></v-checkbox>
+            <v-text-field v-model="multigeraete" :disabled="!activeNetzwerkdrucker" label="Multifunktionsgeräte z.B. Netzwerkdrucker" type="number" class="pr-5"></v-text-field>
+            <v-text-field v-model="multigeraeteToner" :disabled="!activeNetzwerkdrucker" label="Anzahl verbrauchter Toner" type="number"></v-text-field>
+          </v-row>
+          <v-row>
+            <v-checkbox v-model="activeLaserdrucker" hide-details></v-checkbox>
+            <v-text-field :disabled="!activeLaserdrucker" label="Anzahl Laser- & Tintenstrahldrucker" type="number" class="pr-5"></v-text-field>
+            <v-text-field :disabled="!activeLaserdrucker" label="Anzahl verbrauchter Toner" type="number"></v-text-field>
+          </v-row>
+          <v-row>
+            <v-checkbox v-model="activeBeamer"></v-checkbox>
+            <v-text-field :disabled="!activeBeamer" label="Anzahl Beamer" type="number"></v-text-field>
+          </v-row>
+          <v-row>
+            <v-text-field label="Papierverbrauch" suffix="kg" type="number"></v-text-field>
+          </v-row>
+        </v-card>
+      </v-form>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    activeNetzwerkdrucker: false,
+    activeLaserdrucker: false,
+    activeBeamer: false,
+    anzahlMitarbeiter: null
+  })
+}
+</script>
+
+<style lang="scss">
+/* Chrome, Safari, Edge, Opera */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+/* Firefox */
+input[type=number] {
+  -moz-appearance: textfield;
+}
+</style>
