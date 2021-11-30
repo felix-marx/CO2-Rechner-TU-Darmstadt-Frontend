e3f61c0a178d0d780cb07d2cde27e2a83eee5221
franzmartin.schlink <franzmartin.schlink@stud.tu-darmstadt.de>
2021-11-30 13:07:03 +0100

    feat(Uebersicht erstellter Umfragen) added dummy overview card for a poll

---
+++ src/components/uebersichtUmfragen.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-container>
+        <v-card
+            elevation="10"
+            outlined
+        >
+            <v-list-item three-line>
+                <v-list-item-content>
+                    <div class="text-overline mb-4">
+                        Zuletzt bearbeitet am XX.YY.ZZZZ
+                    </div>
+                    <v-list-item-title class="text-h5 mb-1">
+                        Umfrage 1
+                    </v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+
+            <v-card-actions>
+                <v-btn
+                    outlined
+                    rounded
+                    text
+                >
+                    bearbeiten
+                </v-btn>
+
+
+
+            </v-card-actions>
+
+        </v-card>
+    </v-container>
+</template>
