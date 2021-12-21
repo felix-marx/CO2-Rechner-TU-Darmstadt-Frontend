dd3044144f2f8a45046ad48dd109e7b684ea8f49
Tobias512 <tobi-wehr@web.de>
2021-12-21 16:00:20 +0100

    fix: linter

--- src/views/nutzerauswertung.vue
+++ src/views/nutzerauswertung.vue
@@ -1,11 +1,15 @@
 <template>
   <v-app>
-    <line-chart :data="chartData"/>
-    <pie-chart :data="[['Blueberry', 44], ['Strawberry', 23]]"></pie-chart>
-    <column-chart :data="[['Sun', 32], ['Mon', 46], ['Tue', 28]]"></column-chart>
-    <bar-chart :data="[['X-Small', 5], ['Small', 27]]"></bar-chart>
-    <area-chart :data="{'2017-01-01 00:00:00 -0800': 2, '2017-01-01 00:01:00 -0800': 5}"></area-chart>
-    <scatter-chart :data="[[174.0, 80.0], [176.5, 82.3]]" xtitle="Size" ytitle="Population"></scatter-chart>
+    <line-chart :data="chartData" />
+    <pie-chart :data="[['Blueberry', 44], ['Strawberry', 23]]" />
+    <column-chart :data="[['Sun', 32], ['Mon', 46], ['Tue', 28]]" />
+    <bar-chart :data="[['X-Small', 5], ['Small', 27]]" />
+    <area-chart :data="{'2017-01-01 00:00:00 -0800': 2, '2017-01-01 00:01:00 -0800': 5}" />
+    <scatter-chart
+      :data="[[174.0, 80.0], [176.5, 82.3]]"
+      xtitle="Size"
+      ytitle="Population"
+    />
   </v-app>
 </template>
 
