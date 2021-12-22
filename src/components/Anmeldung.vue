fd3cf9ede1658be287e34cc15ccd4394fb380a5a
Anhilly <anton.hillmann@stud.tu-darmstadt.de>
2021-12-22 17:34:28 +0100

    feat(Anmeldung): Added new Login page

--- src/components/Anmeldung.vue
+++ src/components/Anmeldung.vue
@@ -1,123 +1,82 @@
 <template>
-  <v-dialog
-    v-model="dialog"
-    width="500"
-  >
-    <template v-slot:activator="{ on, attrs }">
-      <!-- Zeigt an welcher Nutzer angemeldet ist -->
-      <v-col
-        v-if="showAnmelden"
-        cols="2"
+  <v-app>
+    <Header />
+    <v-main>
+      <v-card
+        elevation="2"
+        outlined
       >
-        <h4>
-          Angemeldet als: {{ getCookieAttribut("email") }}
-        </h4>
-      </v-col>
-      <!-- Anmelde Button -->
-      <v-col
-        v-if="!showAnmelden"
-      >
-        <v-btn
-          text
-          v-bind="attrs"
-          v-on="on"
-        >
-          <span class="mr-2">Anmelden</span>
-          <v-icon>mdi-account</v-icon>
-        </v-btn>
-      </v-col>
-      <v-col v-if="showAnmelden">
-        <v-btn
-          text
-          @click="deleteAbmelden()"
-        >
-          <span class="mr-2">Abmelden</span>
-          <v-icon>mdi-account</v-icon>
-        </v-btn>
-      </v-col>
-    </template>
-
-    <v-card class="d-flex justify-center">
-      <v-container>
         <v-card-title class="justify-center">
-          {{ istRegistrierung ? "Registrierung" : "Anmeldung" }}
+          Anmeldung
         </v-card-title>
-        <v-row>
-          <v-text-field
-            v-model="username"
-            class="px-5"
-            :rules="requiredRule"
-            label="E-Mail Adresse"
-            prepend-icon="mdi-account"
-            required
-          />
-        </v-row>
-        <v-row>
-          <v-text-field
-            v-model="password"
-            class="px-5"
-            :rules="passwordRule.concat(requiredRule)"
-            label="Passwort"
-            type="password"
-            prepend-icon="mdi-key"
-          />
-        </v-row>
-        <v-row v-if="istRegistrierung">
-          <v-text-field
-            v-model="rePassword"
-            class="px-5"
-            :rules="passwordRule.concat(requiredRule)"
-            label="Passwort"
-            type="password"
-            prepend-icon="mdi-key"
-            hint="Mindestens 8 Zeichen"
-          />
-        </v-row>
-        <v-row
-          v-if="istRegistrierung"
-          class="px-5"
-        >
-          <v-checkbox
-            v-model="agbBestaetigt"
-            label="Ich erkläre mich mit den AGB von TU-Darmstadt einverstanden."
-          />
-        </v-row>
-        <!-- Error Message on wrong user input and error in backend -->
-        <v-row v-if="errorMessage != null && errorMessage != ''">
-          <p
-            class="mx-12"
-          >
-            {{ errorMessage }}
-          </p>
-        </v-row>
-        <v-row>
-          <v-col class="px-10">
-            <v-btn
-              color="primary"
-              @click="postAnmeldung()"
-            >
-              <span>Anmelden</span>
-              <v-icon>mdi-account</v-icon>
-            </v-btn>
-          </v-col>
-          <v-col class="px-10">
+        <v-container>
+          <v-row>
+            <v-col />
+            <v-col>
+              <v-text-field
+                v-model="username"
+                :rules="requiredRule"
+                label="E-Mail Adresse"
+                prepend-icon="mdi-account"
+                required
+              />
+            </v-col>
+            <v-col />
+          </v-row>
+          <v-row>
+            <v-col />
+            <v-col>
+              <v-text-field
+                v-model="password"
+                :rules="passwordRule.concat(requiredRule)"
+                label="Passwort"
+                type="password"
+                prepend-icon="mdi-key"
+              />
+            </v-col>
+            <v-col />
+          </v-row>
+          <v-row>
+            <v-col class="px-10">
+              <v-btn
+                color="primary"
+                :style="{ left: '50%', transform: 'translateX(-50%)' }"
+                @click="postAnmeldung()"
+              >
+                <span>Anmelden</span>
+                <v-icon>mdi-account</v-icon>
+              </v-btn>
+            </v-col>
+          </v-row>
+          <v-row>
             <v-btn
               color="error"
+              :style="{ left: '50%', transform: 'translateX(-50%)' }"
               @click="postRegistrierung()"
             >
-              <span>Registrieren</span>
+              <span>Neues Konto erstellen</span>
               <v-icon>mdi-account</v-icon>
             </v-btn>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-card>
-  </v-dialog>
+          </v-row>
+        </v-container>
+      </v-card>
+    </v-main>
+    <Footer />
+  </v-app>
 </template>
 
 <script>
+import Footer from "../components/Footer.vue";
+import Header from "../components/Header.vue";
+
 export default {
   name: "Anmeldung",
+
+  components: {
+    Header,
+    Footer
+  }
+  ,
 
   data: () => ({
     username: null,
@@ -138,7 +97,7 @@ export default {
   }),
 
   computed: {
-    showAnmelden:  function() {
+    showAnmelden: function () {
       return this.checkIfCookieAttributExists("email")
     },
   },
@@ -158,17 +117,17 @@ export default {
     /**
      * Checks if identifier is set in cookie
      */
-    checkIfCookieAttributExists: function(identifier) {
+    checkIfCookieAttributExists: function (identifier) {
       return document.cookie
-      .split(";")
-      .some((item) => item.trim().startsWith(identifier))
+        .split(";")
+        .some((item) => item.trim().startsWith(identifier))
     },
 
     /**
      * Gets the value of the identifier set in the cookie if it is set else null
      */
-    getCookieAttribut: function(identifier) {
-      if(this.checkIfCookieAttributExists(identifier)) {
+    getCookieAttribut: function (identifier) {
+      if (this.checkIfCookieAttributExists(identifier)) {
         return document.cookie
           .split("; ")
           .find(row => row.startsWith(identifier))
@@ -180,7 +139,7 @@ export default {
     /**
      * Deletes the value stored at the identifer in cookie
      */
-    deleteCookieAttribut: function(identifier) {
+    deleteCookieAttribut: function (identifier) {
       //By setting the cookie expire date to an past date it automatically gets deleted
       document.cookie = identifier + "=; SameSite=Lax; expires=Thu, 18 Dec 2013 12:00:00 UTC"
     },
@@ -189,20 +148,20 @@ export default {
      * Checks if the user input is valid and returns true if valid
      * Otherwise false and sets errorMessage to user fault
      */
-    checkValidInput: function(registrierung) {
+    checkValidInput: function (registrierung) {
       if (!this.username || !this.password) {
         this.errorMessage = "Unvollständige Angabe"
         return false
       }
-      if(registrierung && this.password != this.rePassword) {
+      if (registrierung && this.password != this.rePassword) {
         this.errorMessage = "Passwort stimmt nicht überein"
         return false
       }
-      if(this.username.length < 5) {
+      if (this.username.length < 5) {
         this.errorMessage = "Email Mindestlänge ist 5 Zeichen"
         return false
       }
-      if(this.password.length < 8) {
+      if (this.password.length < 8) {
         this.errorMessage = "Passwort Mindestlänge ist 8 Zeichen"
         return false
       }
@@ -212,8 +171,8 @@ export default {
 
     postAnmeldung: async function () {
       //User input validation and set error message
-      this.istRegistrierung = false 
-      if(!this.checkValidInput(this.istRegistrierung)) {
+      this.istRegistrierung = false
+      if (!this.checkValidInput(this.istRegistrierung)) {
         return
       }
 
@@ -230,12 +189,12 @@ export default {
         .then((response) => response.json())
         .then((data) => {
           //This is always the case when the backend returns a package
-          if(data.status == "success") {
+          if (data.status == "success") {
             this.setCookie("sessiontoken", data.data.sessiontoken)
             this.setCookie("email", this.username)
           }
           //Message on success or error send from Backend
-          this.errorMessage = (data.status == "success") ?  data.data.message : data.error.message
+          this.errorMessage = (data.status == "success") ? data.data.message : data.error.message
           console.log("Success:", data)
         })
         .catch((error) => {
@@ -247,10 +206,10 @@ export default {
     postRegistrierung: async function () {
       //User input validation and set error message
       this.istRegistrierung = true
-      if(!this.checkValidInput(this.istRegistrierung)) {
+      if (!this.checkValidInput(this.istRegistrierung)) {
         return
       }
-      
+
       await fetch("http://localhost:9000/auth/registrierung", {
         method: "POST",
         headers: {
@@ -264,12 +223,12 @@ export default {
         .then((response) => response.json())
         .then((data) => {
           //This is always the case when the backend returns a package
-          if(data.status == "success") {
+          if (data.status == "success") {
             this.setCookie("sessiontoken", data.data.sessiontoken)
             this.setCookie("email", this.username)
-          } 
+          }
           //Message on success or error send from Backend 
-          this.message = (data.status == "success") ?  data.data.message : data.error.message
+          this.message = (data.status == "success") ? data.data.message : data.error.message
           console.log("Success:", data)
         })
         .catch((error) => {
@@ -277,8 +236,8 @@ export default {
           console.error("Error:", error)
         });
     },
-    
-    deleteAbmelden: async function() {
+
+    deleteAbmelden: async function () {
       await fetch("http://localhost:9000/auth/abmeldung", {
         method: "DELETE",
         headers: {
@@ -294,7 +253,7 @@ export default {
           //Delete cookie and log if not success
           this.deleteCookieAttribut("email")
           this.deleteCookieAttribut("sessiontoken")
-          if(data.status != "success") {
+          if (data.status != "success") {
             console.log("Server konnte nicht löschen")
           }
 
