7b995f2f003c3b5c3aae413629977616ef741789
Anhilly <anton.hillmann@stud.tu-darmstadt.de>
2021-12-27 14:25:29 +0100

    feat(router): checks postCheckAnmeldung before redirecting to survey

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,29 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Umfrage from '../components/umfrage.vue'
 //import Home from '../views/Home.vue'
 import Anmeldung from '../components/Anmeldung.vue'
 
 Vue.use(VueRouter)
+
+let status = false;
 
 const routes = [
   {
     path: '/',
     name: 'Anmeldung',
     component: Anmeldung
+  },
+  {
+    path: '/survey',
+    name: 'umfrage',
+    component: Umfrage,
+    beforeEnter: (to, from, next) => {
+      postCheckAnmeldung()  
+      console.log(status)
+      if(status) next();
+      else router.push('*')
+    }
   },
   {
     path: '/survey/:umfrageID',
@@ -37,3 +51,56 @@ const router = new VueRouter({
 })
 
 export default router
+
+async function  postCheckAnmeldung() {
+  //User input validation and set error message
+  status = true
+  /*
+  await fetch("http://localhost:9000/auth/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username: getCookieAttribut('email'),
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      //This is always the case when the backend returns a package
+      if (data.status == "success") {
+        console.log("Erfolgreiche anmeldung, sollte weitergeleitet werden")
+        return true;
+      } else {
+        console.log("Fehler digga")
+        return false;
+      }
+    })
+    .catch((error) => {
+      //This is always the case when the backend returns nothing -> Timeout
+      console.error("Error:", error)
+    }); */
+}
+
+/*
+function getCookieAttribut(identifier) {
+  if(this.checkIfCookieAttributExists(identifier)) {
+    console.log(document.cookie
+      .split("; ")
+      .find(row => row.startsWith(identifier))
+      .split("=")[1])
+    return document.cookie
+      .split("; ")
+      .find(row => row.startsWith(identifier))
+      .split("=")[1]
+  }
+  return null
+}
+
+function checkIfCookieAttributExists(identifier) {
+  return document.cookie
+  .split(";")
+  .some((item) => item.trim().startsWith(identifier))
+}
+*/
+
