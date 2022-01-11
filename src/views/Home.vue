72cacaf53d6f11238468069a205b896ad685e215
Anhilly <anton.hillmann@stud.tu-darmstadt.de>
2022-01-11 15:50:13 +0100

    refactor: Changing text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -38,7 +38,7 @@ export default {
     currentTabType: Umfrage,
     tabList: [
         { id: 0, title: "CO2 Fussabdruck", componentType: Umfrage },
-        { id: 1, title: "Ergebnisse", componentType: Uebersicht },
+        { id: 1, title: "Umfragen übersicht", componentType: Uebersicht },
     ],
   }),
 
