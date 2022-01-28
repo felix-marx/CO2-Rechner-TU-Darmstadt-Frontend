1f19099b74fc27beec30b9e66fb710732465f78b
Anhilly <anton.hillmann@stud.tu-darmstadt.de>
2022-01-28 23:03:41 +0100

    feat(Mail): Rebuild Mail Authentifizierungs page

--- src/components/MailAuthentifizierung.vue
+++ src/components/MailAuthentifizierung.vue
@@ -1,11 +1,38 @@
 <template>
   <v-app>
     <Header />
-    <v-container>
-      <v-card class="pa-7 mt-2">
-        <v-card-title>Bitte bestätigen Sie ihre E-Mail Adresse.</v-card-title>
-      </v-card>
-    </v-container>
+    <v-main>
+      <v-container v-if="displayLoadingAnimation">
+        <LoadingAnimationVue />
+      </v-container>
+
+      <v-container
+        v-if="emailConfirmed"
+        class="text-center"
+      >
+        <v-card>
+          <h2 class="pa-7 mt-2">
+            Ihre E-Mail Adresse wurde erfolgreich bestätigt.
+          </h2>
+          <v-btn
+            color="primary"
+            large
+            @click="$router.push({ path:'/'})"
+          >
+            <v-icon left>
+              mdi-account
+            </v-icon>
+            <span>Zurück zur Anmeldung</span>
+          </v-btn>
+        </v-card>
+      </v-container>
+
+      <v-container v-if="emailNotConfirmed">
+        <v-card class="pa-7 mt-2">
+          <h2>Leider ist ein Fehler aufgetreten. Bitte probiere Sie es erneut oder wenden sich an den Administrator.</h2>
+        </v-card>
+      </v-container>
+    </v-main>
     <Footer />
   </v-app>
 </template>
@@ -13,40 +40,47 @@
 <script>
 import Footer from "./Footer.vue";
 import Header from "./Header.vue";
+import LoadingAnimationVue from "./componentParts/loadingAnimation.vue";
 
 export default {
 
-    components: { Footer, Header },
-    data: () => ({
-    }),
-    created() {
-        // request the year of the umfrage
-        this.postMailBestaetigung(this.$route.params.userID)
-    },
-    methods: {
-        postMailBestaetigung: async function (userID){
-            await fetch(process.env.VUE_APP_BASEURL + "/auth/emailBestaetigung", {
-            method: "POST",
-            headers: {
-            "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                nutzerID: userID
-            }),
+  components: { Footer, Header, LoadingAnimationVue },
+  data: () => ({
+    displayLoadingAnimation: true,
+    emailConfirmed: false,
+    emailNotConfirmed: false,
+  }),
+
+  created() {
+    this.postMailBestaetigung(this.$route.params.nutzerID)
+  },
+
+  methods: {
+    postMailBestaetigung: async function (userID) {
+      await fetch(process.env.VUE_APP_BASEURL + "/auth/emailBestaetigung", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          nutzerID: userID
+        }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data)
+          //This is always the case when the backend returns a package
+          if (data.status == "success") {
+            this.emailConfirmed = true
+            this.displayLoadingAnimation = false
+            //this.$router.push('/').catch(() => { })
+          }
         })
-            .then((response) => response.json())
-            .then((data) => {
-            console.log(data)
-            //This is always the case when the backend returns a package
-            if (data.status == "success") {
-                this.$router.push('/').catch(() => { })
-            }
-            })
-            .catch((error) => {
-            //This is always the case when the backend returns nothing -> Timeout
-            console.error("Error:", error)
-            });
+        .catch((error) => {
+          //This is always the case when the backend returns nothing -> Timeout
+          console.error("Error:", error)
+        });
     },
-    }
+  }
 }
 </script>
