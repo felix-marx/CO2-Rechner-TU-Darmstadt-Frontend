3cc46df16cc9fde7e14fd14d1b079682b0f03704
Felix <62305832+Lithium-1Hauptgruppe@users.noreply.github.com>
2022-01-31 11:09:57 +0100

    feat(umfrage): Improved userfeedback

--- src/components/umfrage.vue
+++ src/components/umfrage.vue
@@ -15,7 +15,7 @@
         <p>
           Die gesamte Berechnung bezieht sich immer auf ein vollständig abgeschlossenes Kalenderjahr.
         </p>
-        <p>Der erste Teil des CO2-Rechner erfragt allgemeine Angaben über Ihre TU-Einheit, wie beispielsweise Anzahl der Mitarbeitenden, Standort Ihrer Einheit und gemeinschaftlich genutzte IT-Geräte. Wenn Sie diesen ersten Teil beantwortet haben, klicken Sie auf "<i>Speichern &amp; Link generieren</i>". Dadurch wird ein Link generiert, der zu der Umfrage für die Mitarbeitenden führt. Schicken Sie diesen Link an alle Mitarbeitende Ihrer TU-Einheit. Eine Mailvorlage finden Sie unter dem Link.</p>
+        <p>Der erste Teil des CO2-Rechner erfragt allgemeine Angaben über Ihre TU-Einheit, wie beispielsweise Anzahl der Mitarbeitenden, Standort Ihrer Einheit und gemeinschaftlich genutzte IT-Geräte. Sollten Sie keine genauen Angaben kennen, geben Sie bitte eine grobe Schätzung an. Wenn Sie diesen ersten Teil beantwortet haben, klicken Sie auf "<i>Speichern &amp; Link generieren</i>". Dadurch wird ein Link generiert, der zu der Umfrage für die Mitarbeitenden führt. Schicken Sie diesen Link an alle Mitarbeitende Ihrer TU-Einheit. Eine Mailvorlage finden Sie unter dem Link.</p>
         <p>Hinter einigen Fragen befindet sich ein Fragezeichensymbol, dort finden Sie zusätzliche Hinweise und Informationen, die zur Beantwortung der Frage hilfreich sind.  </p>
         <p>Falls Sie zu einem späteren Zeitpunkt nochmals Angaben ändern oder vervollständigen möchten, können Sie in der linken oberen Ecke „<i>Umfragenübersicht</i>“ auswählen, um auf den ersten Teil des CO2-Rechners wieder zuzugreifen. Bitte klicken Sie am Ende auf „Speichern“ um ihre Änderungen final einzutragen. </p>
         <p>
@@ -94,7 +94,10 @@
 
           <br>
           <h3>
-            Welche Gebäude nutzt Ihre Abteilung?
+            Welche Gebäude nutzt Ihre Abteilung (<a
+              href="https://www.tu-darmstadt.de/universitaet/campus/index.de.jsp"
+              target="_blank"
+            >Campus Start</a>)?
             <Tooltip
               tooltip-text="Alle Gebäude beginnen je nach Standort mit den Buchstaben S, B, L,
               H oder W. Die Autovervollständigung sollte Ihnen dabei helfen."
@@ -125,7 +128,7 @@
                   v-model="objekt[1]"
                   :rules="absolutpositivRules"
                   :min="0"
-                  label="Nutzfläche"
+                  label="Nutzfläche des ausgewählten Gebäudes"
                   prepend-icon="mdi-domain"
                   type="number"
                   suffix="qm"
