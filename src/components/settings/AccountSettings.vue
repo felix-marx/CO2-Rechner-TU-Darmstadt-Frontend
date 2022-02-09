19aa067e4bb7f3bc59788faab77f84f8610c710f
tobnie <niehues.tobias@gmail.com>
2022-02-09 12:44:51 +0100

    style(UserSettings): changed layout in account settings

--- src/components/settings/AccountSettings.vue
+++ src/components/settings/AccountSettings.vue
@@ -1,193 +1,197 @@
 <template>
-  <v-card
-    elevation="2"
-    outlined
-  >
-    <v-card-title>
-      Account Löschen
-    </v-card-title>
-    <v-card class="pa-7">
-      <v-row>
-        <v-col cols="10">
-          Hier können Sie ihren Account löschen. Wenn Sie sicher sind, dies tun zu wollen, drücken Sie bitte den folgenden Knopf.
-        </v-col>
-        <v-col cols="2">
-          <v-dialog
-            v-model="showDeleteDialog"
-            transition="dialog-bottom-transition"
-          >
-            <template v-slot:activator="{ on, attrs }">
-              <!-- Mit diesem Button soll der Account gelöscht werden. -->
-              <v-col class="text-right">
-                <v-btn
-                  class="ma2"
-                  outlined
-                  text
-                  color="delete"
-                  v-bind="attrs"
-                  v-on="on"
-                >
-                  <v-icon>mdi-delete-outline</v-icon>
-                  Löschen
-                </v-btn>
-              </v-col>
-            </template>
-
-            <v-card>
-              <v-toolbar
-                color="primary"
-                dark
-              >
-                Löschen des Accounts
-              </v-toolbar>
-              <v-card-text>
-                <p class="pt-6">
-                  Sind Sie sicher, dass Sie Ihren Account löschen möchten?
-                  <br/>
-                  <b>Diese Aktion kann nicht zurückgenommen werden und alle Ihre Umfragen werden gelöscht! </b>
-                </p>
-            
-                <div>
-                  Geben Sie zur Bestätigung bitte Ihre Email-Adresse in folgendes Feld ein:
-                </div>
-                <v-text-field
-                  v-model="usernameConfirmation"
-                  outlined
-                />
-              </v-card-text>
-
-              <v-divider />
-
-              <v-card-actions>
-                <LoadingAnimation
-                  v-if="displayLoadingAnimation"
-                />
-                <v-alert
-                  v-if="accountDeleted && signedOut && !displayLoadingAnimation"
-                  type="success"
-                >
-                  Ihr Account wurde erfolgreich gelöscht. Sie können dieses Fenster nun schließen.
-                </v-alert>
-                <v-alert
-                  v-if="deleteRequestError && !displayLoadingAnimation"
-                  type="error"
-                >
-                  Ihr Account konnte nicht gelöscht werden. Bitte versuchen Sie es erneut.
-                </v-alert>
-                <v-spacer />
-                <v-btn
-                  :disabled="!checkUsernameConfirmation || accountDeleted"
-                  color="delete"
-                  text
-                  @click="deleteAccountAndSignout()"
-                >
-                  Ich bestätige
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
-        </v-col>
-      </v-row>
-    </v-card>
-
+  <v-container>
     <v-card
       elevation="2"
-      class="py-4"
       outlined
     >
-      <div class="text-center">
-        <v-card-title class="justify-center">
-          Kennwort Ändern
-        </v-card-title>
-      </div>
-
-      <v-container>
+      <v-card-title>
+        Account Löschen
+        <v-divider />
+      </v-card-title>
+      <v-card
+        class="pl-7 pr-7"
+        elevation="0"
+      >
         <v-row>
-          <v-col />
-          <v-col cols="16">
-            <v-text-field
-              v-model="password"
-              class="px-5"
-              label="Aktuelles Passwort"
-              type="password"
-              prepend-icon="mdi-key"
-            />
+          <v-col cols="10">
+            <div class="mt-4">
+              Hier können Sie Ihren Account löschen. Wenn Sie sicher sind, dies tun zu wollen, drücken Sie bitte den folgenden Knopf.
+            </div>
           </v-col>
-          <v-col />
-        </v-row>
-        <v-row>
-          <v-col />
-          <v-col cols="16">
-            <v-text-field
-              v-model="newPassword"
-              class="px-5"
-              :rules="passwordRule.concat(requiredRule)"
-              label="Neues Passwort"
-              type="password"
-              prepend-icon="mdi-key"
-              hint="Mindestens 8 Zeichen"
-            />
-          </v-col>
-          <v-col />
-        </v-row>
-        <v-row>
-          <v-col />
-          <v-col cols="16">
-            <v-text-field
-              v-model="newPasswordRe"
-              class="px-5"
-              :rules="passwordRule.concat(requiredRule)"
-              label="Neues Passwort wiederholen"
-              type="password"
-              prepend-icon="mdi-key"
-            />
-          </v-col>
-          <v-col />
-        </v-row>
-        <v-row class="text-center">
-          <v-col />
-          <v-col
+          <v-col cols="2">
+            <v-dialog
+              v-model="showDeleteDialog"
+              transition="dialog-bottom-transition"
+            >
+              <template v-slot:activator="{ on, attrs }">
+                <!-- Mit diesem Button soll der Account gelöscht werden. -->
+                <v-col class="text-right">
+                  <v-btn
+                    outlined
+                    text
+                    color="delete"
+                    v-bind="attrs"
+                    v-on="on"
+                  >
+                    <v-icon>mdi-delete-outline</v-icon>
+                    Löschen
+                  </v-btn>
+                </v-col>
+              </template>
+              <v-card>
+                <v-toolbar
+                  color="primary"
+                  dark
+                >
+                  Löschen des Accounts
+                </v-toolbar>
+                <v-card-text>
+                  <p class="pt-6">
+                    Sind Sie sicher, dass Sie Ihren Account löschen möchten?
+                    <br>
+                    <b>Diese Aktion kann nicht zurückgenommen werden und alle Ihre Umfragen werden gelöscht! </b>
+                  </p>
             
-            cols="7"
-          >
-            <v-alert
-              v-if="changedPasswordStatus === 1"
-              outlined
-              type="success"
-              text
-            >
-              Ihr Kennwort wurde erfolgreich geändert!
-            </v-alert>
-            <v-alert
-              v-if="changedPasswordStatus === 2"
-              outlined
-              type="error"
-              text
-            >
-              Leider ist etwas schief gegangen, bitte wenden Sie sich an den Administrator oder probieren Sie es erneut.
-            </v-alert>
+                  <div>
+                    Geben Sie zur Bestätigung bitte Ihre Email-Adresse in folgendes Feld ein:
+                  </div>
+                  <v-text-field
+                    v-model="usernameConfirmation"
+                    outlined
+                  />
+                </v-card-text>
+
+                <v-divider />
+
+                <v-card-actions>
+                  <LoadingAnimation
+                    v-if="displayLoadingAnimation"
+                  />
+                  <v-alert
+                    v-if="accountDeleted && signedOut && !displayLoadingAnimation"
+                    type="success"
+                  >
+                    Ihr Account wurde erfolgreich gelöscht. Sie können dieses Fenster nun schließen.
+                  </v-alert>
+                  <v-alert
+                    v-if="deleteRequestError && !displayLoadingAnimation"
+                    type="error"
+                  >
+                    Ihr Account konnte nicht gelöscht werden. Bitte versuchen Sie es erneut.
+                  </v-alert>
+                  <v-spacer />
+                  <v-btn
+                    :disabled="!checkUsernameConfirmation || accountDeleted"
+                    color="delete"
+                    text
+                    @click="deleteAccountAndSignout()"
+                  >
+                    Ich bestätige
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
           </v-col>
-          <v-col />
         </v-row>
-        <v-row>
-          <v-col class="text-center py-3">
-            <v-btn
-              color="blue"
-              @click="postPasswortAendern()"
+      </v-card>
+      
+      <v-card-title>
+          Kennwort Ändern  
+          <v-divider />
+        </v-card-title>
+      <v-card
+        class="pl-7 pr-7"
+        elevation="0"
+      >
+        Hier können sie Ihr Kennwort ändern.
+        <v-container>
+          <v-row>
+            <v-col />
+            <v-col cols="16">
+              <v-text-field
+                v-model="password"
+                class="px-5"
+                label="Aktuelles Passwort"
+                type="password"
+                prepend-icon="mdi-key"
+              />
+            </v-col>
+            <v-col />
+          </v-row>
+          <v-row>
+            <v-col />
+            <v-col cols="16">
+              <v-text-field
+                v-model="newPassword"
+                class="px-5"
+                :rules="passwordRule.concat(requiredRule)"
+                label="Neues Passwort"
+                type="password"
+                prepend-icon="mdi-key"
+                hint="Mindestens 8 Zeichen"
+              />
+            </v-col>
+            <v-col />
+          </v-row>
+          <v-row>
+            <v-col />
+            <v-col cols="16">
+              <v-text-field
+                v-model="newPasswordRe"
+                class="px-5"
+                :rules="passwordRule.concat(requiredRule)"
+                label="Neues Passwort wiederholen"
+                type="password"
+                prepend-icon="mdi-key"
+              />
+            </v-col>
+            <v-col />
+          </v-row>
+          <v-row class="text-center">
+            <v-col />
+            <v-col
+            
+              cols="7"
             >
-              <v-icon
-                color="white"
-                left
+              <v-alert
+                v-if="changedPasswordStatus === 1"
+                outlined
+                type="success"
+                text
               >
-                mdi-content-save-outline
-              </v-icon>
-              <span class="white--text">Kennwort ändern!</span>
-            </v-btn>
-          </v-col>
-        </v-row>
-      </v-container>
+                Ihr Kennwort wurde erfolgreich geändert!
+              </v-alert>
+              <v-alert
+                v-if="changedPasswordStatus === 2"
+                outlined
+                type="error"
+                text
+              >
+                Leider ist etwas schief gegangen, bitte wenden Sie sich an den Administrator oder probieren Sie es erneut.
+              </v-alert>
+            </v-col>
+            <v-col />
+          </v-row>
+          <v-row>
+            <v-col class="text-center py-3">
+              <v-btn
+                color="blue"
+                @click="postPasswortAendern()"
+              >
+                <v-icon
+                  color="white"
+                  left
+                >
+                  mdi-content-save-outline
+                </v-icon>
+                <span class="white--text">Kennwort ändern</span>
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card>
     </v-card>
-  </v-card>
+  </v-container>
 </template>
 
 <script>
