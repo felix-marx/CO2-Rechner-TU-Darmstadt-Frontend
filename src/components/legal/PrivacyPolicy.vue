7f7c4ce2be3e9468f8ea35ba3de7b86b9a0da3ae
Anton <salync@protonmail.com>
2023-05-08 01:06:11 +0200

    feat(i18n): Added german text for the privacypolicy

--- src/components/legal/PrivacyPolicy.vue
+++ src/components/legal/PrivacyPolicy.vue
@@ -8,10 +8,10 @@
       <v-container>
         <v-card elevation="0">
           <p>
-            Diese Datenschutzerklärung dient zur Erfüllung der nach Artikel 13 EU DSGVO geforderten Informationspflicht bei Erhebung von Daten zum Zeitpunkt der Erhebung bei betroffenen Personen.
-          </p>
+            {{ $t('legal.PrivacyPolicy.DatenschutzerklaerungHeadline') }}
+         </p>
       
-          <h2>Name und Anschrift des Verantwortlichen</h2>
+          <h2>{{ $t('legal.PrivacyPolicy.NameUndAnschrift') }}</h2>
           <p>
             Technische Universität Darmstadt<br>
             Karolinenplatz 5<br>
@@ -21,9 +21,9 @@
               target="_blank"
               href="https://www.tu-darmstadt.de"
             >www.tu-darmstadt.de</a><br>
-            Bei Anfragen oder Beschwerden zum Datenschutz können Sie sich mit den Datenschutzbeauftragten der Technischen Universität Darmstadt in Verbindung setzen.
+            {{ $t('legal.PrivacyPolicy.AnfrageDatenschutz') }}
           </p>
-          <h2>Kontaktdaten der Datenschutzbeauftragten</h2>
+          <h2> {{ $t('legal.PrivacyPolicy.KontaktdatenDatenschutzBeauftragten')}} </h2>
           <p>
             Technische Universität Darmstadt<br>
             Der behördliche Datenschutzbeauftragte<br>
@@ -35,11 +35,11 @@
               target="_blank"
             >www.tu-darmstadt.de/datenschutz</a>
           </p>
-          <h2>Rechte und Beschwerdemöglichkeiten</h2>
+          <h2>{{ $t('legal.PrivacyPolicy.RechteBeschwerde_0')}}</h2>
           <p>
-            Sie haben das Recht sich bei datenschutzrechtlichen Problemen bei der zuständigen Aufsichtsbehörde zu beschweren.<br>
-            Kontaktadresse der Aufsichtsbehörde der Technischen Universität Darmstadt:<br>
-            Der Hessische Datenschutzbeauftragte<br>
+            {{ $t('legal.PrivacyPolicy.RechteBeschwerde_1')}} <br>
+            {{ $t('legal.PrivacyPolicy.RechteBeschwerde_2')}} <br>
+            {{ $t('legal.PrivacyPolicy.RechteBeschwerde_3')}} <br>
             Postfach 3163<br>
             65021 Wiesbaden<br>
             <a
@@ -49,130 +49,130 @@
             Telefon: +49 611 1408-0<br>
             Telefax: +49 611 1408-611<br><br>
 
-            Sie haben gegenüber der Technischen Universität Darmstadt folgende Rechte hinsichtlich Ihrer gespeicherten personenbezogenen Daten:<br>
+            {{ $t('legal.PrivacyPolicy.RechteDaten')}}<br>
             <v-list
               dense
             >
-              <v-list-item-title>Recht auf Auskunft,</v-list-item-title>
-              <v-list-item-title>Recht auf Berichtigung oder Löschung,</v-list-item-title>
-              <v-list-item-title>Recht auf Einschränkung der Verarbeitung,</v-list-item-title>
-              <v-list-item-title>Recht auf Widerruf Ihrer Einwilligung,</v-list-item-title>
-              <v-list-item-title>Recht auf Widerspruch gegen die Verarbeitung,</v-list-item-title>
-              <v-list-item-title>Recht auf Datenübertragbarkeit, in einer gängigen, strukturierten und maschinenlesbaren Form (ab dem 25. Mai 2018).</v-list-item-title>
+              <v-list-item-title>{{ $t('legal.PrivacyPolicy.RechtAuskunft')}}</v-list-item-title>
+              <v-list-item-title>{{ $t('legal.PrivacyPolicy.RechtLoeschung')}}</v-list-item-title>
+              <v-list-item-title>{{ $t('legal.PrivacyPolicy.RechtVerarbeitung')}}</v-list-item-title>
+              <v-list-item-title>{{ $t('legal.PrivacyPolicy.RechtWiederruf')}}</v-list-item-title>
+              <v-list-item-title>{{ $t('legal.PrivacyPolicy.RechtWiderspruch')}}</v-list-item-title>
+              <v-list-item-title>{{ $t('legal.PrivacyPolicy.RechtDatenuebertragbarkeit')}}</v-list-item-title>
             </v-list>
           </p>
 
-          <h2>Art der gespeicherten Daten, Zweck und Rechtsgrundlagen, Löschungsfristen</h2>
+          <h2>{{ $t('legal.PrivacyPolicy.ZweckRechtsgrundlagenLoeschungsfrist')}}</h2>
           
-          <h3>Umgang mit personenbezogenen Daten</h3>
+          <h3>{{ $t('legal.PrivacyPolicy.UmgangPersonDaten_0')}}</h3>
           <p>
-            Personenbezogene Daten sind Informationen, mit deren Hilfe eine natürliche Person bestimmbar ist, also Angaben, durch die Personen identifizierbar sind. Dazu gehören insbesondere Namen, E-Mail-Adressen, Personal-, Matrikelnummern oder Telefonnummern.<br>
-            Aber auch Daten über Vorlieben, Hobbies, Mitgliedschaften oder auch Informationen über Webseiten, die aufgesucht wurden, zählen zu personenbezogenen Daten.<br>
-            Personenbezogene Daten werden von uns nur dann erhoben, genutzt und weitergegeben, wenn dies gesetzlich erlaubt ist oder die Nutzer in die Datenerhebung eingewilligt haben.<br>
+            {{ $t('legal.PrivacyPolicy.UmgangPersonDaten_1')}}<br>
+            {{ $t('legal.PrivacyPolicy.UmgangPersonDaten_2')}}<br>
+            {{ $t('legal.PrivacyPolicy.UmgangPersonDaten_3')}}<br>
           </p>
-          <h3>Zugriffsdaten/Server-Logdateien</h3>
-          <p>
-            Beim Zugriff auf die Seiten dieses Webservers werden im Allgemeinen folgende Daten in den Server-Logfiles gespeichert:<br>
+          <h3>{{ $t('legal.PrivacyPolicy.Logdateien_0') }}</h3>
+          {{ $t('legal.PrivacyPolicy.Logdateien_1') }}<p>
+            <br>
      
             <v-list dense>
-              <v-list-item-title>IP-Adresse</v-list-item-title>
-              <v-list-item-title>Datum und Uhrzeit</v-list-item-title>
-              <v-list-item-title>Typ des Client Browsers</v-list-item-title>
-              <v-list-item-title>URL der aufgerufenen Seite</v-list-item-title>
-              <v-list-item-title>Gegebenenfalls die Fehlermeldung zum aufgetretenen Fehler</v-list-item-title>
-              <v-list-item-title>Gegebenenfalls der anfragende Provider</v-list-item-title>
+              <v-list-item-title>{{ $t('legal.PrivacyPolicy.Logdateien_2') }}</v-list-item-title>
+              <v-list-item-title>{{ $t('legal.PrivacyPolicy.Logdateien_3') }}</v-list-item-title>
+              <v-list-item-title>{{ $t('legal.PrivacyPolicy.Logdateien_4') }}</v-list-item-title>
+              <v-list-item-title>{{ $t('legal.PrivacyPolicy.Logdateien_5') }}</v-list-item-title>
+              <v-list-item-title>{{ $t('legal.PrivacyPolicy.Logdateien_6') }}</v-list-item-title>
+              <v-list-item-title>{{ $t('legal.PrivacyPolicy.Logdateien_7') }}</v-list-item-title>
             </v-list>
 
-            Diese Daten dienen ausschließlich zum Zwecke der Kontrolle der Funktionalität, der Sicherheit und Fehlerbehebung.<br>
-            Diese Nutzung basiert auf EU DSGVO Artikel 6 Absatz 1 c).<br>
-            Alle Logdateien werden automatisiert nach spätestens 10 Tagen gelöscht oder anonymisiert.<br>
+          {{ $t('legal.PrivacyPolicy.Logdateien_8') }}<br>
+          {{ $t('legal.PrivacyPolicy.Logdateien_9') }}<br>
+          {{ $t('legal.PrivacyPolicy.Logdateien_10') }}<br>
           </p>
 
-          <h3>Kontaktaufnahme</h3>
+          <h3>{{ $t('legal.PrivacyPolicy.Kontaktaufnahme_0') }}</h3>
           <p>
-            Zur Kontaktaufnahme mit Mitgliedern der Technischen Universität Darmstadt (zum Beispiel per Kontaktformular oder E-Mail) werden Ihre Angaben zwecks Bearbeitung der Anfrage sowie für den Fall, dass Anschlussfragen entstehen, gespeichert.<br>
-            Nach Bearbeitung Ihres Anliegens bzw. nach Erfüllung der Rechtspflicht oder des genutzten Dienstes werden die Daten gelöscht, es sei denn, die Aufbewahrung der Daten ist zur Umsetzung berechtigter Interessen der Technischen Universität Darmstadt oder auf Grund einer gesetzlichen Vorschrift (z.B. Gesetz, Rechtsverordnung, Satzung der Technischen Universität etc.) erforderlich.<br>
+            {{ $t('legal.PrivacyPolicy.Kontaktaufnahme_1') }}<br>
+            {{ $t('legal.PrivacyPolicy.Kontaktaufnahme_2') }}<br>
           </p>
 
-          <h3>Cookies</h3>
+          <h3>{{ $t('legal.PrivacyPolicy.Cookies_0') }}</h3>
           <p>
-            Cookies sind kleine Dateien, die es ermöglichen, auf Ihrem Zugriffsgerät (PC, Smartphone o.ä.) spezifische, auf das Gerät bezogene Informationen zu speichern.<br>
-            Sie dienen der Benutzerfreundlichkeit von Webseiten (z.B. Speicherung von Login-Daten). <br>
-            Sie können auf den Einsatz der Cookies Einfluss nehmen. <br>
-            Die meisten Browser verfügen über eine Option, mit der das Speichern von Cookies eingeschränkt oder komplett verhindert werden kann.<br>
-            Allerdings wird darauf hingewiesen, dass die Nutzung und insbesondere der Nutzungskomfort ohne Cookies eingeschränkt sein können. <br>
-            Zur Nutzung der login-gesicherten Seiten sind Cookies zwingend erforderlich. <br>
-            Sie dienen zur Feststellung der Zugangsberechtigung und werden nach Beenden der Session gelöscht.
+            {{ $t('legal.PrivacyPolicy.Cookies_1') }}<br>
+            {{ $t('legal.PrivacyPolicy.Cookies_2') }}<br>
+            {{ $t('legal.PrivacyPolicy.Cookies_3') }}<br>
+            {{ $t('legal.PrivacyPolicy.Cookies_4') }}<br>
+            {{ $t('legal.PrivacyPolicy.Cookies_5') }}<br>
+            {{ $t('legal.PrivacyPolicy.Cookies_6') }}<br>
+            {{ $t('legal.PrivacyPolicy.Cookies_7') }}
           </p>
 
-          <h3>Zugriffsgeschützte Webseiten und benutzerbasierte Dienste</h3>
+          <h3>{{ $t('legal.PrivacyPolicy.ZugriffsgeschuetzteWebsiten_0') }}</h3>
           <p>
-            Neben den oben genannten Daten werden zusätzlich zur Berechtigungskontrolle des Datenzugriffs der Benutzername oder eine Kennung erhoben. <br>
-            Diese Daten werden spätestens nach 10 Tagen gelöscht oder anonymisiert, es sei denn, die Aufbewahrung der Daten ist zur Umsetzung berechtigter Interessen der Technischen Universität Darmstadt oder einer gesetzlichen Vorschrift (z.B. Gesetz, Rechtsverordnung, Satzungen der Technischen Universität Darmstadt, etc.) erforderlich.<br>
-            Für alle Dienste, die nicht unter diese Vorgaben fallen, werden explizit Einwilligungserklärungen eingeholt. <br>
-            Die Löschung erfolgt hier je nach Vorgabe des genutzten Dienstes. <br>
-            Die Löschungsfrist wird im Rahmen des Verzeichnisses zur Verarbeitungstätigkeiten eindeutig definiert. <br>
-            Die Rechtsgrundlage ist hier EU DSGVO Artikel 6 Absatz 1a).
+            {{ $t('legal.PrivacyPolicy.ZugriffsgeschuetzteWebsiten_1') }}
           </p>
 
-          <h3>Einbindung von Diensten Dritter</h3>
+          <h3>{{ $t('legal.PrivacyPolicy.EinbindungDienstenDritter_0') }}</h3>
           <p>
-            Zum Versenden von E-Mails verwendet diese Webseite die Dienste des <a
+            {{ $t('legal.PrivacyPolicy.EinbindungDienstenDritter_1') }} <a
               href="https://www.hrz.tu-darmstadt.de/hrz_aktuelles/index.de.jsp"
               target="_blanck"
             >Hochschulrechenzentrums Darmstadt</a> (kurz HRZ).<br>
-            E-Mails versenden wir nur zum Zweck der E-Mail-Validierung, und wenn Sie eine Zurücksetzung Ihres Passwortes anfordern. <br>
-            Zur Erfüllung dieser Aufgabe teilen wir Ihre gespeicherte E-Mail-Adresse mit dem Anbieter.<br>
-            Zur Passwortwiederherstellung, versenden wir ein neues Passwort an die in dem Konto hinterlegte E-Mail-Adresse.<br>
-            Da das HRZ die E-Mails versendet, wird deren Inhalt, d.h. in diesem Fall auch das neu gesetzte Passwort, mit dem Dienstleister geteilt.<br>
-            Deshalb sind Sie aufgefordert unverzüglich nach Ihrer Anmeldung Ihr Passwort zu ändern.
+            {{ $t('legal.PrivacyPolicy.EinbindungDienstenDritter_2') }}<br>
+            {{ $t('legal.PrivacyPolicy.EinbindungDienstenDritter_3') }}<br>
+            {{ $t('legal.PrivacyPolicy.EinbindungDienstenDritter_4') }}<br>
+            {{ $t('legal.PrivacyPolicy.EinbindungDienstenDritter_5') }}<br>
+            {{ $t('legal.PrivacyPolicy.EinbindungDienstenDritter_6') }}
+
           </p>
 
-          <h3>Erstellung einer Umfrage</h3>
+          <h3>{{ $t('legal.PrivacyPolicy.ErstellungUmfrage_0') }}</h3>
           <p>
-            Zum Erstellen einer Umfrage ist das Anlegen eines Nutzeraccounts notwendig.<br>
-            Für den Nutzeraccount müssen Sie eine gültige E-Mail der Technischen Universität Darmstadt (z.B. max.mustermann@tu-darmstadt.de) angeben.<br>
-            Die zum Erzeugen der der Umfrage eingegebenen Daten sind in einer Weise abstrahiert, dass Rückschlüsse auf individuelle Personen nicht möglich sind.<br>
-            Die zum Erstellen einer Umfrage durch Sie eingegebenen Daten können Sie jederzeit löschen.<br>
-            Sie entscheiden, wie lange die Daten in unserem System vorhanden sind.<br><br>
- 
-            Die von uns erhobenen Daten werden nicht an Dritte weitergegeben.<br>
+            {{ $t('legal.PrivacyPolicy.ErstellungUmfrage_1') }}<br>
+            {{ $t('legal.PrivacyPolicy.ErstellungUmfrage_2') }}<br>
+            {{ $t('legal.PrivacyPolicy.ErstellungUmfrage_3') }}<br>
+            {{ $t('legal.PrivacyPolicy.ErstellungUmfrage_4') }}<br>
+            {{ $t('legal.PrivacyPolicy.ErstellungUmfrage_5') }}<br><br>
 
-            Wir erfassen bei der Erstellung der Umfrage folgende Daten von Ihnen:
+              {{ $t('legal.PrivacyPolicy.ErstellungUmfrage_6') }}<br>
+
+            {{ $t('legal.PrivacyPolicy.ErstellungUmfrage_7') }}
             <v-list dense>
-              <v-list-item-title>Frei wählbare Umfragebezeichnung</v-list-item-title>
-              <v-list-item-title>Bilanzierungsjahr</v-list-item-title>
-              <v-list-item-title>Mitarbeitendenanzahl</v-list-item-title>
-              <v-list-item-title>angegebene Gebäude und deren Nutzfläche</v-list-item-title>
-              <v-list-item-title>Anzahl der gewählten IT-Geräte </v-list-item-title>
-            </v-list>	
-            Wir ordnen diese Daten dem absendenden Nutzerkonto zu. Dazu speichern wir:
+              <v-list-item-title>{{ $t('legal.PrivacyPolicy.ErstellungUmfrage_7_1') }}</v-list-item-title>
+              <v-list-item-title>{{ $t('legal.PrivacyPolicy.ErstellungUmfrage_7_2') }}</v-list-item-title>
+              <v-list-item-title>{{ $t('legal.PrivacyPolicy.ErstellungUmfrage_7_3') }}</v-list-item-title>
+              <v-list-item-title>{{ $t('legal.PrivacyPolicy.ErstellungUmfrage_7_4') }}</v-list-item-title>
+              <v-list-item-title>{{ $t('legal.PrivacyPolicy.ErstellungUmfrage_7_5') }}</v-list-item-title>
+            </v-list>
+            {{ $t('legal.PrivacyPolicy.ErstellungUmfrage_8_0') }}
             <v-list dense>
-              <v-list-item-title>E-Mail Adresse als Nutzerkennung</v-list-item-title>
-              <v-list-item-title>Unkenntliche Repräsentation des Passwortes (Hash)</v-list-item-title>
+              <v-list-item-title>{{ $t('legal.PrivacyPolicy.ErstellungUmfrage_8_1') }}</v-list-item-title>
+              <v-list-item-title>{{ $t('legal.PrivacyPolicy.ErstellungUmfrage_8_2') }}</v-list-item-title>
             </v-list>
 
-            Bei der Durchführung der Umfrage werden weitere Daten anonymisiert erhoben:
+            {{ $t('legal.PrivacyPolicy.ErstellungUmfrage_9_0') }}
             <v-list dense>
-              <v-list-item-title>Gewähltes Verkehrsmedium mit Pendelstrecke</v-list-item-title>
-              <v-list-item-title>Anzahl an Fahrgemeinschaftsmitgliedern</v-list-item-title>
-              <v-list-item-title>Arbeitstage im Büro</v-list-item-title>
-              <v-list-item-title>Angegebene Dienstreisen mit Reisestrecke</v-list-item-title>
-              <v-list-item-title>Anzahl der gewählten IT-Geräte</v-list-item-title>
+              <v-list-item-title>{{ $t('legal.PrivacyPolicy.ErstellungUmfrage_9_1') }}</v-list-item-title>
+              <v-list-item-title>{{ $t('legal.PrivacyPolicy.ErstellungUmfrage_9_2') }}</v-list-item-title>
+              <v-list-item-title>{{ $t('legal.PrivacyPolicy.ErstellungUmfrage_9_3') }}</v-list-item-title>
+              <v-list-item-title>{{ $t('legal.PrivacyPolicy.ErstellungUmfrage_9_4') }}</v-list-item-title>
+              <v-list-item-title>{{ $t('legal.PrivacyPolicy.ErstellungUmfrage_9_5') }}</v-list-item-title>
             </v-list>
+            {{ $t('legal.PrivacyPolicy.ErstellungUmfrage_10') }}
+            <br>
 
-            Personenbezogene Daten werden bei der Beantwortung der Fragen nicht erhoben.<br>
+            {{ $t('legal.PrivacyPolicy.ErstellungUmfrage_11') }}<sub>2</sub>{{ $t('legal.PrivacyPolicy.ErstellungUmfrage_12') }}<br>
+            {{ $t('legal.PrivacyPolicy.ErstellungUmfrage_13') }}
+            {{ $t('legal.PrivacyPolicy.ErstellungUmfrage_14') }}
 
-            Die Ergebnisse der Umfrage werden im CO<sub>2</sub>-Rechner gespeichert und dort den Umfrage-Verantwortlichen zur Verfügung gestellt.<br>
-            Die Ergebnisse sind so weit anonymisiert, dass Rückschlüsse auf einzelne Personen nicht möglich sind.<br>
-            Die Löschung der Ergebnisse erfolgt durch die Umfrage-Verantwortlichen.
+            <br>
+
           </p>
 
-          <h3>Umfrageauswertung</h3>
+          <h3>{{ $t('legal.PrivacyPolicy.Umfrageauswertung_0') }}</h3>
           <p>
-            Die Auswertung der Umfrageergebnisse ist durch Sie jederzeit möglich.<br>
-            Sie können die Auswertung mit Dritten teilen, indem Sie dies in der Auswertung aktivieren.<br>
-            Diese Freigabe kann durch Sie jederzeit widerrufen werden.
+            {{ $t('legal.PrivacyPolicy.Umfrageauswertung_1') }}<br>
+            {{ $t('legal.PrivacyPolicy.Umfrageauswertung_2') }}<br>
+            {{ $t('legal.PrivacyPolicy.Umfrageauswertung_3') }}<br>
+
           </p>
         </v-card>
       </v-container>
