1882a4d91dc34951380636b02e6f4eef061130e2
Tobias Wehrle <tobi-wehr@web.de>
2023-09-12 10:36:16 +0200

    feat: removed old code from landing page

--- src/components/landingpage/Login.vue
+++ src/components/landingpage/Login.vue
@@ -14,41 +14,7 @@
           <v-card-title class="justify-center">
             <h2>TU Darmstadt CO<sub>2</sub>-Rechner</h2>
           </v-card-title>Willkommen bei dem TU Darmstadt CO<sub>2</sub>-Rechner. Mit diesem können Sie die CO<sub>2</sub>-Emissionen von TU-Einheiten berechnen.
-          <br> Erstellen Sie einfach ein Konto, um eine Umfrage für Ihre TU-Einheit zu erstellen.
-          <v-card-title
-            class="justify-center"
-          >
-            {{ istRegistrierung ? "Registrierung" : "Anmeldung" }}
-          </v-card-title>
         </div>
-        <!-- <v-container>
-          <v-row>
-            <v-col v-if="!$keycloak.authenticated">
-              <v-btn @click="login">
-                Login            
-              </v-btn>
-            </v-col>
-            <v-col v-if="$keycloak.authenticated">
-              <v-btn @click="logout">
-                Logout            
-              </v-btn>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col>
-              <v-btn @click="sendRequest2">
-                OK           
-              </v-btn>
-            </v-col>
-            <v-col>
-              <v-btn @click="sendRequest">
-                Hello           
-              </v-btn>
-            </v-col>
-          </v-row>
-        </v-container> -->
-
-        <!-- Signin -->
         <v-container>
           <v-row>
             <v-col
@@ -149,89 +115,5 @@ export default {
     Header,
     Footer
   },
-
-  data: () => ({
-    username: null,
-    password: null,
-    rePassword: null,
-    istRegistrierung: false,
-    bestaetigungAnzeigen: false,
-    agbBestaetigt: false,
-    dialog: false,
-    //Für Fehlermeldung bei Response
-    errorMessage: null,
-    datenschutzlink: process.env.VUE_APP_URL + "/datenschutz",
-
-    requiredRule: [
-      v => !!v || "Muss angegeben werden",
-    ],
-    passwordRule: [
-      v => (v && v.length >= 8) || 'Mindestens 8 Zeichen'
-    ]
-  }),
-
-  methods: {
-    // sendRequest: async function() {
-    //   await fetch(process.env.VUE_APP_BASEURL + "/hello", {
-    //     method: 'GET',
-    //     headers: {
-    //       "Authorization": "Bearer " + this.$keycloak.token,
-    //     }
-    //   }).then(response => {
-    //     if (response.status === 200) {
-    //       response.text().then(text => {
-    //         alert(text);
-    //       });
-    //     } else {
-    //       alert("Error");
-    //     }
-    //   });
-    // },
-    // sendRequest2: async function() {
-    //   await fetch(process.env.VUE_APP_BASEURL + "/", {
-    //     method: 'GET',
-    //     headers: {
-    //       "Authorization": "Bearer " + this.$keycloak.token,
-    //     }
-    //   }).then(response => {
-    //     if (response.status === 200) {
-    //       response.text().then(text => {
-    //         alert(text);
-    //       });
-    //     } else {
-    //       alert("Error");
-    //     }
-    //   });
-    // },
-    /**
-     * Checks if the user input is valid and returns true if valid
-     * Otherwise false and sets errorMessage to user fault
-     */
-    checkValidInput: function (registrierung) {
-      if (!this.username || !this.password) {
-        this.errorMessage = "Unvollständige Angabe"
-        return false
-      }
-      if (registrierung && this.password != this.rePassword) {
-        this.errorMessage = "Passwort stimmt nicht überein"
-        return false
-      }
-      if (this.username.length < 5) {
-        this.errorMessage = "E-Mail Mindestlänge ist 5 Zeichen"
-        return false
-      }
-      var regex = /.+@(.*\.)?tu-darmstadt\.de/
-      if (registrierung && !regex.test(this.username)) {
-        this.errorMessage = "Bitte geben Sie eine gültige TU E-Mail an"
-        return false
-      }
-      if (this.password.length < 8) {
-        this.errorMessage = "Passwort Mindestlänge ist 8 Zeichen"
-        return false
-      }
-      this.errorMessage = null
-      return true
-    },
-  }
 };
 </script>
