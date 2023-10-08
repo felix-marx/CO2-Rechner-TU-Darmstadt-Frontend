4b848020337d7f4c52d55646e49310e704e24787
Anton <salync@protonmail.com>
2023-10-08 09:23:53 +0200

    feat: Finished i18n for ResultSharingDisabling

--- src/components/evaluation/ResultSharingDisabled.vue
+++ src/components/evaluation/ResultSharingDisabled.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="text-center">
     <h3 class="text-center">
-      Es tut uns leid, aber die Umfrage mit ID
-      "{{ this.$route.params.umfrageID }}" hat das Teilen der Auswertung deaktiviert.
+      {{ $t('evaluation.ResultSharingDisabled.ResultSharingDisabled_0') }}
+      "{{ this.$route.params.umfrageID }}" {{ $t('evaluation.ResultSharingDisabled.ResultSharingDisabled_1') }}
     </h3>
     <p>
-      Bitte wenden Sie sich an die Person, von der Sie den Link für die Auswertung erhalten haben.  
+      {{ $t('evaluation.ResultSharingDisabled.ResultSharingDisabled_2') }}
     </p>
   </div>
 </template>
