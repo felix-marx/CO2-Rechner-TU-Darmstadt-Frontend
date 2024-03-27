4df3cd409b7832135b7979e46416e3023cbbc03c
Tobias Wehrle <tobi-wehr@web.de>
2024-03-27 07:35:40 +0100

    fix: variable rename

--- src/components/evaluation/SurveyEvaluation.vue
+++ src/components/evaluation/SurveyEvaluation.vue
@@ -359,7 +359,7 @@ export default {
   data() {
     return {
       constants: constants,
-      responsesuccessful: false,
+      responseSuccessful: false,
       responseNotSuccessful: false,
       responsedata: {
         id: null,
