7b8f0278065dd21f85f5dc62cebcbe3ebe320545
Tobias Wehrle <tobi-wehr@web.de>
2024-05-18 11:26:49 +0200

    fix: changed v-list-item-title for mobile

--- src/components/userSurveyManagement/EditSurvey.vue
+++ src/components/userSurveyManagement/EditSurvey.vue
@@ -82,12 +82,9 @@
                           v-for="(problem, index) in errorTextArray.required"
                           :key="index"
                         >
-                          <v-list-item-content>
-                            <v-list-item-title
-                              class="text-sm-body-2 delete--text font-weight-black"
-                              v-text="problem"
-                            />
-                          </v-list-item-content>
+                          <label class="text-sm-body-2 delete--text font-weight-black mb-2">
+                            {{ problem }}
+                          </label>
                         </v-list-item>
                       </v-list>
                     </div>
@@ -103,10 +100,9 @@
                           :key="index"
                         >
                           <v-list-item-content>
-                            <v-list-item-title
-                              class="text-sm-body-2"
-                              v-text="problem"
-                            />
+                            <label class="text-sm-body-2 mb-2">
+                              {{ problem }}
+                            </label>
                           </v-list-item-content>
                         </v-list-item>
                       </v-list>
